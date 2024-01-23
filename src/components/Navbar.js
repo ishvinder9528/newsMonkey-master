3f5bb87ded261c56a39e335f3dfedb8a8c23c194
Ishvinder Singh <ishvinder133@gmail.com>
2024-01-23 17:33:19 +0000

    some nessecary changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ const Navbar =()=>{
         <nav className="navbar fixed-top navbar-expand-lg bg-dark navbar-dark">
           <div className="container-fluid">
             <Link className="navbar-brand" to="/">
-              Navbar
+              News-Monkey
             </Link>
             <button
               className="navbar-toggler"
